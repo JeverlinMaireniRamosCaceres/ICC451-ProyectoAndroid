83250d6e3efb43d3af698736f9431719792fbb92
Jeverlin <jmrc0005@ce.pucmm.edu.do>
2026-09-25 16:15:42 -0400

    Se hizo la pantalla de chat

--- app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/users/UsuariosActivity.java
+++ app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/users/UsuariosActivity.java
@@ -1,4 +1,14 @@
 package com.example.proyectoandroid_chatfirebase.ui.users;
 
-public class UsuariosActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.proyectoandroid_chatfirebase.R;
+
+public class UsuariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuarios);
+    }
 }
