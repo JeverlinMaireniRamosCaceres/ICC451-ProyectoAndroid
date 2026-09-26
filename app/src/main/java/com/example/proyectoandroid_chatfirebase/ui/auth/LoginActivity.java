9bcd1eceae49e5388e60327207d28788f4e63221
VG <EAVG0001@ce.pucmm.edu.do>
2026-09-25 21:15:36 -0400

    Crear LoginActivity y RegistroActivity con ViewBinding

--- app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/auth/LoginActivity.java
@@ -1,4 +1,19 @@
 package com.example.proyectoandroid_chatfirebase.ui.auth;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyectoandroid_chatfirebase.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
--- app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/auth/RegistroActivity.java
+++ app/src/main/java/com/example/proyectoandroid_chatfirebase/ui/auth/RegistroActivity.java
@@ -1,4 +1,19 @@
 package com.example.proyectoandroid_chatfirebase.ui.auth;
 
-public class RegistroActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyectoandroid_chatfirebase.databinding.ActivityRegistroBinding;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    private ActivityRegistroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegistroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
